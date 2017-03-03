2833e9ce643fef1e2fd0aafc95cce814659d96ea
Long N. Le <longle1@illinois.edu>
2017-03-03 16:59:43 -0600

    added protection macros in _kiss_fft_guts.h

--- _kiss_fft_guts.h
+++ _kiss_fft_guts.h
@@ -1,3 +1,5 @@
+#ifndef _KISS_FFT_GUTS_H
+#define _KISS_FFT_GUTS_H
 /*
 Copyright (c) 2003-2010, Mark Borgerding
 
@@ -161,4 +163,6 @@ struct kiss_fft_state{
 #else
 #define  KISS_FFT_TMP_ALLOC(nbytes) KISS_FFT_MALLOC(nbytes)
 #define  KISS_FFT_TMP_FREE(ptr) KISS_FFT_FREE(ptr)
+#endif
+
 #endif
